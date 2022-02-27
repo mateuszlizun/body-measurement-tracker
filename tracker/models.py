c375d5ad511ecc4a38f1013d9ac7db95a23f70c8
mateuszlizun <mateusz@lizun.pl>
2022-02-27 16:46:57 +0100

    Add measurement model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Measurement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(
+        "date published", default=timezone.now)
+    chest = models.DecimalField(max_digits=5, decimal_places=2)
+    waist = models.DecimalField(max_digits=5, decimal_places=2)
+    hips = models.DecimalField(max_digits=5, decimal_places=2)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
