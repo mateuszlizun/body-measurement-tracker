5bde891488fae72b0920cf5e18f8e99ac128adb9
mateuszlizun <mateusz@lizun.pl>
2022-03-02 17:25:31 +0100

    MeasurementListView: calculate data diffs when data is not null only

--- tracker/views.py
+++ tracker/views.py
@@ -9,17 +9,24 @@ class MeasurementListView(generic.ListView):
 
     def get_queryset(self):
         # TODO: logged in user ID should be retrieved here
-        measurements = Measurement.objects.filter(
-            user=User.objects.get(pk=1))
+        measurements = Measurement.objects.filter(user=User.objects.get(pk=1))
 
         for index, measurement in enumerate(measurements):
             if index != len(measurements) - 1:
-                measurement.chest_difference = measurement.chest - \
-                    measurements[index + 1].chest
-                measurement.waist_difference = measurement.waist - \
-                    measurements[index + 1].waist
-                measurement.hips_difference = measurement.hips - \
-                    measurements[index + 1].hips
-                measurement.weight_difference = measurement.weight - \
-                    measurements[index + 1].weight
+                if measurement.chest and measurements[index + 1].chest:
+                    measurement.chest_difference = (
+                        measurement.chest - measurements[index + 1].chest
+                    )
+                if measurement.waist and measurements[index + 1].waist:
+                    measurement.waist_difference = (
+                        measurement.waist - measurements[index + 1].waist
+                    )
+                if measurement.hips and measurements[index + 1].hips:
+                    measurement.hips_difference = (
+                        measurement.hips - measurements[index + 1].hips
+                    )
+                if measurement.weight and measurements[index + 1].weight:
+                    measurement.weight_difference = (
+                        measurement.weight - measurements[index + 1].weight
+                    )
         return measurements
