4bc3270d849d407f7ba9dd076c7defc5192feee8
mateuszlizun <mateusz@lizun.pl>
2022-03-08 20:22:57 +0100

    Redirect to login page after logout

--- core/urls.py
+++ core/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="users/login.html"),
         name="login",
     ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", include("tracker.urls")),
 ]
