c7cdd40739c85626ac5c242a7342c87c2c22cfb6
mateuszlizun <mateusz@lizun.pl>
2022-03-11 19:08:16 +0100

    Add profile view

--- core/urls.py
+++ core/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
@@ -23,6 +25,7 @@ from users.forms import CustomSetPasswordForm
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", users_views.register, name="register"),
+    path("profile/", users_views.profile, name="profile"),
     path(
         "login/",
         auth_views.LoginView.as_view(template_name="users/login.html"),
