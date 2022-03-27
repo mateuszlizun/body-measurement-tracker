a75f08becd06c0bfbab4850cb9ea92c353c88a7b
mateuszlizun <mateusz@lizun.pl>
2022-03-27 11:48:35 +0200

    MeasurementCreateUpdateForm - add measurement attributes

--- tracker/forms.py
+++ tracker/forms.py
@@ -8,7 +8,27 @@ from .models import Measurement
 class MeasurementCreateUpdateForm(forms.ModelForm):
     class Meta:
         model = Measurement
-        fields = ["pub_date", "chest", "waist", "hips", "weight"]
+        fields = [
+            "pub_date",
+            "neck",
+            "shoulders",
+            "chest",
+            "bust",
+            "under_bust",
+            "bicep_r",
+            "bicep_l",
+            "forearm_r",
+            "forearm_l",
+            "waist",
+            "abdomen",
+            "hips",
+            "buttocks",
+            "thigh_r",
+            "thigh_l",
+            "calf_r",
+            "calf_l",
+            "weight",
+        ]
         widgets = {
             "pub_date": forms.DateTimeInput(
                 attrs={
